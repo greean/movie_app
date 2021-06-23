5b12e808879c28219707479d3a3bfed9be595eef
Andy Green <aj.green07@outlook.com>
2021-06-23 19:52:08 +0100

    images now display side-by-side

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -6,7 +6,7 @@ const MovieList = (props) => {
     return (
 		<>
 			{props.movies.map((movie, index) => (
-				<div className='image-container d-flex justify-content-start m-3'>
+				<div className='col image-container d-flex justify-content-start m-3'>
 					<img src={movie.Poster} alt={movie.Title}></img>
                     <div 
                         onClick={()=> props.handleFavouritesClick(movie)} 
